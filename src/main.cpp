5c5b5948f9beac3efc9908705284fab131362911
Diana Carolina Gomez <dianaCdenkitronik.com>
2023-03-25 00:23:38 -0500

    Prueba punteros

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <Arduino.h>
-int numero = 10;
-void loop(){
-  Serial.print("En loop: numero =");//en loop() numero =10
-  Serial.println(numero);
-  duplicar(&numero); //pasamos la direccion de numero ala funcion 
+
+
+ int numero = 10;
+ void setup() {
+
+ Serial.print(numero);
+
 }
 
 void duplicar(int *punteroNumero){
   *punteroNumero = *punteroNumero *2;
-  Serial.print("En duplicar()=punteroNumero=");
-  Serial.println(*punteroNumero);
+    Serial.print("En duplicar()=punteroNumero=");
+    Serial.println(*punteroNumero);
+} 
+
+void loop(){
+  Serial.print("En loop: numero =");//en loop() numero =10
+  Serial.println(numero);
+  duplicar( &numero); //pasamos la direccion de numero ala funcion 
+  char institucion[]= {'U','C','E','V','A','\n'};
+    for (int i = 0; i < 6; i++)
+      Serial.print(institucion[i]);
+        //Serial.print(*(institucion + i));
+      //  Serial.print(*(institucion+ i * sizeof(char)));
+      Serial.flush();
+      exit(0);
+   
 }
+
